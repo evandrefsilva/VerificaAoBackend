e4af0515b2f7f36a3b7fbef19978c197c9db2125
luteroelavoco <luteroelavoco90@gmail.com>
2024-10-12 16:26:02 -0300

    save

--- src/Services/Models/DTO/AssociateUserCategoryDTO.cs
+++ src/Services/Models/DTO/AssociateUserCategoryDTO.cs
@@ -6,7 +6,7 @@ namespace Services.Models.DTO
 {
     public class AssociateUserCategoryDTO
     {
-        public int UserId { get; set; }
+        public Guid UserId { get; set; }
         public int CategoryId { get; set; }
     }
 
